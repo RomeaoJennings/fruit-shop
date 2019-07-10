2a85af3aa70e20e4099dc9c06588b6477133d92d
Romeao Jennings <romeao.jennings@gmail.com>
2019-07-10 12:23:54 -0700

    Added category data initialization with DataInitializer

---
+++ src/main/java/com/romeao/fruitshop/bootstrap/DataInitializer.java
@@ -0,0 +1,34 @@
+package com.romeao.fruitshop.bootstrap;
+
+import com.romeao.fruitshop.domain.Category;
+import com.romeao.fruitshop.repository.CategoryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final Logger log = LoggerFactory.getLogger(DataInitializer.class);
+    private final CategoryRepository categoryRepository;
+
+
+    public DataInitializer(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        addCategories();
+    }
+
+    private void addCategories() {
+        categoryRepository.save(new Category("Fruits"));
+        categoryRepository.save(new Category("Dried"));
+        categoryRepository.save(new Category("Fresh"));
+        categoryRepository.save(new Category("Exotic"));
+        categoryRepository.save(new Category("Nuts"));
+        log.info("Added categories: {} records", categoryRepository.count());
+    }
+}
