13ffe7af8dbeee46ddaab97a0b14467dd0186dcd
Romeao Jennings <romeao.jennings@gmail.com>
2019-07-12 08:41:42 -0700

    Add CustomerController and error handlers

--- src/main/java/com/romeao/fruitshop/api/v1/util/Urls.java
+++
@@ -1,7 +0,0 @@
-package com.romeao.fruitshop.api.v1.util;
-
-public class Urls {
-    private static final String API_BASE = "/api/v1/";
-    public static final String CUSTOMERS = API_BASE + "customers/";
-    public static final String CATEGORIES = API_BASE + "categories/";
-}
